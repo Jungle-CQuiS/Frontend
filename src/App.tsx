4bc129bb2d95e1c57b505f7742d3c61249dc93d3
ll5215 <foxlover17@gmail.com>
2024-10-18 20:55:27 +0900

    initial settings

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      landing page
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/main.tsx
@@ -0,0 +1,3 @@
+export default function mainpage(){
+    <div>main page</div>
+}
