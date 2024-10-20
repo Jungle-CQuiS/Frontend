a5d9b35f00182cf3531bd237b6cf37aa091008b5
ll5215 <foxlover17@gmail.com>
2024-10-20 16:29:56 +0900

    Feat: Mainpage

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import GlobalStyle from './globalstyles';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainPage from './pages/mainpage/main';
 import LoginPage from './pages/loginpage/login';
+import MultiPage from './pages/multi/multi';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/main" element={<MainPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/multi" element={<MultiPage />} />
         </Routes>
       </Router>
     </>
