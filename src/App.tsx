29e33f4b27cba3bbcaea28f3e29c2986990def8a
ll5215 <foxlover17@gmail.com>
2024-10-20 15:51:56 +0900

    Feat: Landing page and router settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import LandingPage from './pages/landing/landing';
+import GlobalStyle from './globalstyles';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MainPage from './pages/mainpage/main';
+import LoginPage from './pages/loginpage/login';
 
 function App() {
   return (
-    <div>
-      landing page.!!!!
-    </div>
+    <>
+      <GlobalStyle /> {/* 글로벌 스타일은 최상단에서 적용 */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/main" element={<MainPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
