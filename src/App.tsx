6e33625f7dec2d64407040c55a0f037268330f3f
ll5215 <foxlover17@gmail.com>
2024-10-21 18:44:28 +0900

    Feat: Login for temporary

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainPage from './pages/mainpage/main';
 import LoginPage from './pages/loginpage/login';
 import MultiPage from './pages/multi/multi';
+import Room from './pages/multi/room/room';
+import ProtectedRoute from './pages/loginpage/protected';
 
 function App() {
   return (
@@ -12,10 +14,37 @@ function App() {
       <GlobalStyle />
       <Router>
         <Routes>
+          {/* / 페이지는 자유롭게 접근 가능 */}
           <Route path="/" element={<LandingPage />} />
-          <Route path="/main" element={<MainPage />} />
+
+          {/* 로그인 페이지 */}
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/multi" element={<MultiPage />} />
+
+          {/* 보호된 경로들 */}
+          <Route
+            path="/main"
+            element={
+              <ProtectedRoute>
+                <MainPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/multi"
+            element={
+              <ProtectedRoute>
+                <MultiPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/room/:id"
+            element={
+              <ProtectedRoute>
+                <Room />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </Router>
     </>
