d42b6542b2023e883b707b7a9b0719c833206737
Yeram522 <kyeram522@gmail.com>
2024-10-28 01:49:15 +0900

    Fix : ws to wss

--- src/config/websocket/constants.ts
+++ src/config/websocket/constants.ts
@@ -4,7 +4,7 @@ export const SOCKET_DESTINATIONS = {
             // 구독할 토픽
             SUBSCRIBE: {
                 ROOM_INFO: (roomId: string) => `/topic/rooms/${roomId}/info`,
-                USER_JOIN: `/queue/rooms/join`
+                USER_JOIN: `user/queue/rooms/join`
             },
 
             REQUEST: {
