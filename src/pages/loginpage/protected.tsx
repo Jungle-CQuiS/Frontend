f1026f20598b036c4c71e42569bc9b544cec01a7
yongsoung <dydrkfl223@gmail.com>
2024-10-30 08:49:06 +0900

    Feat: Implement login token expiration

--- src/pages/loginpage/protected.tsx
+++ src/pages/loginpage/protected.tsx
@@ -1,8 +1,25 @@
-import { Navigate } from "react-router-dom";
+import { useEffect } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
   const userToken = localStorage.getItem("AccessToken");
+  const navigate = useNavigate();
 
+  useEffect(() => {
+
+    const handleToken = (event: StorageEvent) => {
+      if (event.key === "AccessToken" && event.newValue === null) {
+        alert("세션이 만료되었습니다. 다시 로그인 해주세요.");
+        navigate('/login', { replace: true });
+      }
+    };
+
+    window.addEventListener('storage', handleToken);
+
+    return () => {
+      window.removeEventListener('storage', handleToken);
+    };
+  }, [navigate]);
   // 로그인 상태가 아니면 로그인 페이지로 리디렉션
   if (!userToken) {
     return <Navigate to="/login" replace />;
