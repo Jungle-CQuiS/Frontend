c8a2b14861ffb170426c07d8e394253300e52f1e
ll5215 <foxlover17@gmail.com>
2024-10-30 21:14:41 +0900

    Trigger Push

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ function App() {
           />
           <Route
             path="/multi"
-            element={
+            element={ 
               <ProtectedRoute>
                 <MultiPage />
               </ProtectedRoute>
