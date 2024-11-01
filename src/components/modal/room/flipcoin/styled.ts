92ddf4c6fdfddd6f9cddbb4e3f9dad8c6a69e911
ll5215 <foxlover17@gmail.com>
2024-11-01 22:32:34 +0900

    Fix: Lottie animation for first attack

--- src/components/modal/room/flipcoin/styled.ts
+++ src/components/modal/room/flipcoin/styled.ts
@@ -1,5 +1,20 @@
 import styled from "styled-components";
 
+export const FlipCoinBackdrop = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(0, 0, 0, 0.5); // 반투명 배경
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 1000;
+`;
+
 export const FlipCoinScreen = styled.div`
     width: 600px;
+    z-index: 1001;
+    position: relative;
 `;
