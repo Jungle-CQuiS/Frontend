eefa77ba6796695f589d8d9143c4ffdfe309015b
ll5215 <foxlover17@gmail.com>
2024-11-02 19:40:38 +0900

    Feat: Publishing mypage

---
+++ src/pages/mypage/styled.ts
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+
+export const MyPageContainer = styled.div`
+    display: flex;
+    gap: 100px;
+    margin-top: 72px;
+`;
+export const MyPageHeader = styled.div`
+    display: flex;
+    gap: 10px;
+    align-items: center;
+`;
+
+export const MyPageHeaderImg = styled.img`
+    width: 24px;
+    height: 24px;
+`;
+
+export const MyPageHeaderTitle = styled.div`
+    font-size: 18px;
+    font-weight: 700;
+`;
+
+export const MyPageWrap = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+`;
+
+export const MyPageLabel = styled.div`
+    font-size: 20px;
+    font-weight: 700;
+    color: #969696;
+`;
