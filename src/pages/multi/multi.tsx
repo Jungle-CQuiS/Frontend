44769724b293c720a9537f34096e80caa5365840
yongsoung <dydrkfl223@gmail.com>
2024-11-05 23:54:59 +0900

    Feat: Implement refresh button

--- src/pages/multi/multi.tsx
+++ src/pages/multi/multi.tsx
@@ -1,41 +1,48 @@
 import { useState } from "react";
 import { CreateRoomModal } from "../../components/modal/room";
-import { MainPageSearchBar, MainPageSearchBarWrap, MainPageTable, MainPageTableThead, MainPageTableTheadTh, MainPageTableTheadTr, MainPageTitleText, MultiPageTitle, MultiPageTitleIcon } from "./styled";
+import { MainPageRefreshButton, MainPageRefreshWrap, MainPageSearchBar, MainPageSearchBarWrap, MainPageTable, MainPageTableThead, MainPageTableTheadTh, MainPageTableTheadTr, MainPageTitleText, MultiPageTitle, MultiPageTitleIcon } from "./styled";
 import { PrimaryButtonMedium } from "../../components/buttons/styled";
 import { Background } from "../../components/background/styled";
 import RoomList from './roomlist/roomlist';
 
-export default function MultiPage(){
+export default function MultiPage() {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [searchTerm, setSearchTerm] = useState("");
 
     const handleOpenModal = () => {
         setIsModalOpen(true);
-      };
+    };
     const handleCloseModal = () => {
         setIsModalOpen(false);
-      };
+    };
     const handleDone = () => {
         setIsModalOpen(false);
     };
     const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
         setSearchTerm(event.target.value);
     };
+    const handleRefresh = () => {
+        window.location.reload();
+    }
 
-    return(
+    return (
         <>
             <Background>
                 <MultiPageTitle>
-                    <MultiPageTitleIcon src="/icons/mdi_users_black.svg" alt="Multi Icon Black"/>
+                    <MultiPageTitleIcon src="/icons/mdi_users_black.svg" alt="Multi Icon Black" />
                     <MainPageTitleText>MULTI MODE</MainPageTitleText>
                 </MultiPageTitle>
                 <MainPageSearchBarWrap>
-                    <MainPageSearchBar 
+                    <MainPageSearchBar
                         placeholder="방 이름으로 검색하기"
                         value={searchTerm}
-                        onChange={handleSearch}/>
+                        onChange={handleSearch} />
+                        <MainPageRefreshWrap >
+                    <MainPageRefreshButton onClick={handleRefresh} src="/icons/refresh.svg" />                
+                </MainPageRefreshWrap>
                     <PrimaryButtonMedium onClick={handleOpenModal}>방 만들기</PrimaryButtonMedium>
                 </MainPageSearchBarWrap>
+                
                 <MainPageTable>
                     <MainPageTableThead>
                         <MainPageTableTheadTr>
@@ -44,12 +51,12 @@ export default function MultiPage(){
                             <MainPageTableTheadTh>인원</MainPageTableTheadTh>
                         </MainPageTableTheadTr>
                     </MainPageTableThead>
-                    
+
                     <RoomList searchTerm={searchTerm} />
 
                 </MainPageTable>
             </Background>
-            <CreateRoomModal 
+            <CreateRoomModal
                 open={isModalOpen}
                 onClose={handleCloseModal}
                 onDone={handleDone}
