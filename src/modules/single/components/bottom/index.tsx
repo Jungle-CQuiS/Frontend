61bb7fd328acf814928b7afe3111df34ba306491
ll5215 <foxlover17@gmail.com>
2024-11-06 18:35:18 +0900

    Feat: Show result of each quiz

--- src/modules/single/components/bottom/index.tsx
+++ src/modules/single/components/bottom/index.tsx
@@ -1,15 +1,50 @@
-import { BlackButtonSmall, SecondaryButtonSmall } from "../../../../components/buttons/styled"
-import { SingleModeBottomContainer, SingleModeBottomInput, SingleModeBottomInputWrap } from "./styled"
+import { useNavigate } from "react-router-dom";
+import { BlackButtonSmall, SecondaryButtonSmall } from "../../../../components/buttons/styled";
+import { useConfirm } from "../../../../components/confirmPopup";
+import { SingleModeBottomContainer, SingleModeBottomInput, SingleModeBottomInputWrap } from "./styled";
 
-export const SingleModeBottomComponent = () => {
-
-    return(
-        <SingleModeBottomContainer>
-            <SingleModeBottomInputWrap>
-                <SingleModeBottomInput placeholder="정답을 입력해주세요"/>
-                <BlackButtonSmall>제출하기</BlackButtonSmall>
-            </SingleModeBottomInputWrap>
-            <SecondaryButtonSmall>나가기</SecondaryButtonSmall>
-        </SingleModeBottomContainer>
-    )
+interface SingleModeBottomComponentProps {
+    onSubmit?: () => void;
+    userAnswer?: string;
+    setUserAnswer?: (answer: string) => void;
+    isMultipleChoice?: boolean;
 }
+
+export const SingleModeBottomComponent = ({ onSubmit, userAnswer, setUserAnswer, isMultipleChoice }: SingleModeBottomComponentProps) => {
+    const confirm = useConfirm();
+    const navigate = useNavigate();
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter" && onSubmit) {
+            onSubmit();
+        }
+    };
+
+    const handleExit = async () => {
+        const confirmed = await confirm("퀴즈를 중단하고 나가시겠습니까?")
+        if (confirmed) {
+            navigate("/single");
+        } 
+    }
+
+    return (
+        <SingleModeBottomContainer>
+            {!isMultipleChoice && (
+                <SingleModeBottomInputWrap>
+                    <SingleModeBottomInput
+                        type="text"
+                        placeholder="정답을 입력하세요"
+                        value={userAnswer}
+                        onChange={(e) => setUserAnswer?.(e.target.value)}
+                        onKeyDown={handleKeyDown}
+                    />
+                    <BlackButtonSmall onClick={onSubmit}>제출하기</BlackButtonSmall>
+                </SingleModeBottomInputWrap>
+            )}
+            {isMultipleChoice && (
+                <BlackButtonSmall onClick={onSubmit}>제출하기</BlackButtonSmall>
+            )}
+            <SecondaryButtonSmall onClick={handleExit}>나가기</SecondaryButtonSmall>
+        </SingleModeBottomContainer>
+    );
+};
---
+++ src/pages/single/result/grading/styled.ts
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const SingleGradingContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 100px;
+    align-items: center;
+`;
+
+export const SingleGradingWrap = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 36px;
+    align-items: center;
+`;
+
+export const SingleGradingimg = styled.img`
+    width: 246px;
+    height: 246px;
+`;
+
+export const SingleGradingResult = styled.div`
+    font-size: 26px;
+    font-weight: 600;
+`;
+
+export const SingleGradingButtonWrap = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 16px;
+`;
+
+export const SingleGradingResultWrap = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+`;
+
+export const SingleGradingResultText = styled.div`
+    font-size: 26px;
+    font-weight: 600;
+    color: #969696;
+`;
