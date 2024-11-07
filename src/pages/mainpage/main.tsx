2725250cc593f37940cfebdb4e7cd250b2345089
ll5215 <foxlover17@gmail.com>
2024-11-07 13:46:10 +0900

    Fix: Fix main page style

--- src/pages/mainpage/main.tsx
+++ src/pages/mainpage/main.tsx
@@ -1,24 +1,42 @@
-import { Link } from "react-router-dom";
-import { MainPageContainer, GameModeButton, GameModeWrapper, Icon } from "./styled";
+import { Link, useNavigate } from "react-router-dom";
+import { MainPageContainer, GameModeButton, GameModeWrapper, Icon, IconWrap, MainPageBottom, MainPageSubText, MainPageText } from "./styled";
 
 
 export default function MainPage(){
+
+    const navigate = useNavigate();
+
+    const handleToSingleMode = () => {
+        navigate("/single");
+    }
+
+    const handleToMultiMode = () => {
+        navigate("/multi");
+    }
+
+
     return (
     <>
         <MainPageContainer>
             <GameModeWrapper>
-                <Link to="/single" style={{ textDecoration: "none" }}>
-                    <GameModeButton>
-                        <Icon src="/icons/mdi_user.svg" alt="Single Play Icon" />
-                        <span>Single Play</span>
+                    <GameModeButton onClick={handleToSingleMode}>
+                        <IconWrap>
+                            <Icon src="/icons/mdi_user.svg" alt="Single Play Icon" />
+                        </IconWrap>
+                        <MainPageBottom>
+                            <MainPageText>싱글모드</MainPageText>
+                            <MainPageSubText>혼자 공부하기</MainPageSubText>
+                        </MainPageBottom>
                     </GameModeButton>
-                </Link>
-                <Link to="/multi" style={{ textDecoration: "none" }}>
-                    <GameModeButton>
-                        <Icon src="/icons/mdi_users.svg" alt="Multi Play Icon" />
-                        <span>Multi Play</span>
+                    <GameModeButton onClick={handleToMultiMode}>
+                        <IconWrap>
+                            <Icon src="/icons/mdi_users.svg" alt="Multi Play Icon" />
+                        </IconWrap>
+                        <MainPageBottom>
+                            <MainPageText>멀티모드</MainPageText>
+                            <MainPageSubText>같이 공부하기</MainPageSubText>
+                        </MainPageBottom>
                     </GameModeButton>
-                </Link>
             </GameModeWrapper>
         </MainPageContainer>
     </>
--- src/pages/mainpage/styled.ts
+++ src/pages/mainpage/styled.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { defaultTransition } from "../../css";
 
 export const MainPageContainer = styled.div`
   width: 100%;
@@ -8,7 +9,7 @@ export const MainPageContainer = styled.div`
   align-items: center;
   flex-direction: column;
   padding-top: 150px;
-  background-image: url('/images/mainpage-background.png');
+  background-image: url('/images/game-background.png');
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
@@ -20,23 +21,19 @@ export const GameModeWrapper = styled.div`
   gap: 145px; 
 `;
 
-export const GameModeButton = styled.button<{ disabled?: boolean }>`
-  width: 378px;
-  height: 378px;
-  background-color: #3B82F6;
+export const GameModeButton = styled.div<{ disabled?: boolean }>`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  border-radius: 16px;
   box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
-  color: white;
-  font-size: 32px;
-  font-weight: bold;
   border: none;
   cursor: pointer;
+  ${defaultTransition};
   &:hover {
     background-color: ${(props) => (props.disabled ? "#3B82F6" : "#2563EB")};
+    transform: scale(1.05);
+    box-shadow: 4px 10px 18px rgba(0, 0, 0, 0.25);
   }
 
   ${(props) =>
@@ -53,4 +50,31 @@ export const Icon = styled.img`
   height: 220px;
   margin-bottom: 12px;
   
+`;
+
+export const IconWrap = styled.div`
+  padding: 14px 74px;
+  background: linear-gradient( #2F69FF 22%, #1C3F99 100%);
+  opacity: 90%;
+`;
+
+export const MainPageBottom = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  align-items: center;
+  justify-content: center;
+  padding: 73px 129px;
+  background-color: #fff;
+`;
+
+export const MainPageText = styled.div`
+  font-size: 32px;
+  font-weight: 600;
+`;
+
+export const MainPageSubText = styled.div`
+  color: #969696;
+  font-size: 15px;
+  font-weight: 600;
 `;
