6022eb50a23ed411765b623c6cbe080d938a9245
ll5215 <foxlover17@gmail.com>
2024-11-07 12:05:43 +0900

    Fix: Fix style for mypage

--- src/modules/mypage/components/left/styled.ts
+++ src/modules/mypage/components/left/styled.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const MyPageLeftWrap = styled.div`
     display: flex;
     flex-direction: column;
-    gap: 36px;
+    justify-content: space-between;
 `;
 
 
