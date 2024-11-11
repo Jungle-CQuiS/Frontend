3f82768e7fff144cae3458f2b941fd91c3f45494
Yeram522 <kyeram522@gmail.com>
2024-11-11 17:08:06 +0900

    Feat: 수비팀 리더 선택 실시간 공유 구현

--- src/modules/quiz/components/multi/Answer/AnswerSelect.tsx
+++ src/modules/quiz/components/multi/Answer/AnswerSelect.tsx
@@ -7,6 +7,8 @@ import { Quiz } from "../../../../../types/quiz";
 import { useGameState } from "../../../../../contexts/GameStateContext/useGameState";
 import { useGameUser } from "../../../../../contexts/GameUserContext/useGameUser";
 import { useTeamState } from "../../../../../contexts/TeamStateContext/useTeamState";
+import { useStompContext } from "../../../../../contexts/StompContext";
+import { gameRoomSocketEvents } from "../../../../../hook/gameRoomSocketEvents";
 interface Answer {
   value: string;
   isSelected: boolean;
@@ -19,8 +21,9 @@ interface SelectAnswerPageProps {
 
 export default function AnswerSelectComponent( { selectedQuiz ,userAnswers }: SelectAnswerPageProps) {
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const { getDefenceFinalAnswer } = useGameState();
+    const { _roomId,getDefenceFinalAnswer ,selectedQuizId ,initLeaderSelectQuizeId } = useGameState();
     const {user} = useGameUser();
+    const {stompClient} = useStompContext();
     const {attackTeam} = useTeamState();
     const [answers, setAnswers] = useState<Answer[]>(() => 
         userAnswers?.map((item , index) => ({
@@ -51,6 +54,12 @@ export default function AnswerSelectComponent( { selectedQuiz ,userAnswers }: Se
         setIsModalOpen(false);
     };
 
+    // Leader가 선택한 것을 APP으로 보낸다. 소켓 함수에 넣을 함수.
+    const updateLeaderSelect = (leaderSelect: number) => {
+        // 리더가 선택 한 답을 서버로 보낸다.
+        gameRoomSocketEvents.defSelectQuiz(stompClient, _roomId,leaderSelect);
+    }
+
     return (
         <AnswerSelectContainer>
             <BlackButtonSmall onClick={handleOpenModal}>문제보기</BlackButtonSmall>
@@ -59,7 +68,13 @@ export default function AnswerSelectComponent( { selectedQuiz ,userAnswers }: Se
                     <AnswerSelectRow key={index} onClick={() => handleSelect(index)}>
                         <AnswerSelectCheckbox 
                             className="click-sound"
-                            src={answer.isSelected ? "/icons/checkbox_filled.svg" : "/icons/checkbox_base.svg"} 
+                            src={index === selectedQuizId? "/icons/checkbox_filled.svg" : "/icons/checkbox_base.svg"} 
+                            onClick={() => {
+                                if (user?.isLeader){ //Leader만 선택 가능하다.
+                                    initLeaderSelectQuizeId(index);
+                                    // 소켓 통신으로 다른 팀원들과 선택한 것 공유
+                                    updateLeaderSelect(index);}
+                            }}
                         />
                         <AnswerSelectText>{answer.value}</AnswerSelectText>
                     </AnswerSelectRow>
