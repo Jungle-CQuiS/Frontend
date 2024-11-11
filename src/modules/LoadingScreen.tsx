a05283f8810e584f960176478493c40c86901b24
Yeram522 <kyeram522@gmail.com>
2024-11-11 19:42:33 +0900

    Fix: LoadingScreen style 수정

--- src/modules/LoadingScreen.tsx
+++ src/modules/LoadingScreen.tsx
@@ -2,10 +2,10 @@ import lottie from 'lottie-web'
 import { useEffect, useRef } from "react";
 import { LoadingScreenContainer, LoadingTitle } from './styled';
 
-interface LoadingScreenProp{
-    loadingText ?: string;
+interface LoadingScreenProp {
+    loadingText?: string;
 }
-export const LoadingScreen = ({loadingText = ""} : LoadingScreenProp) => {
+export const LoadingScreen = ({ loadingText = "" }: LoadingScreenProp) => {
 
     // likecontainer의 타입을 명시적으로 지정 (HTMLDivElement | null)
     const likecontainer = useRef<HTMLDivElement | null>(null);
@@ -14,7 +14,7 @@ export const LoadingScreen = ({loadingText = ""} : LoadingScreenProp) => {
     useEffect(() => {
         if (likecontainer.current) {
 
-            const animationData =  require("../lottie/page_loading.json")
+            const animationData = require("../lottie/page_loading.json")
 
             // lottie 애니메이션 로드
             animationInstance.current = lottie.loadAnimation({
@@ -36,12 +36,12 @@ export const LoadingScreen = ({loadingText = ""} : LoadingScreenProp) => {
 
     return (
         <>
-        <LoadingTitle>
-            {loadingText}
-        </LoadingTitle>
-        <LoadingScreenContainer>
-            <div ref={likecontainer}></div>
-        </LoadingScreenContainer>
+            <LoadingScreenContainer>
+                <LoadingTitle>
+                    {loadingText}
+                </LoadingTitle>
+                <div ref={likecontainer}></div>
+            </LoadingScreenContainer>
         </>
     );
 }
--- src/modules/styled.ts
+++ src/modules/styled.ts
@@ -1,26 +1,26 @@
 import styled from "styled-components";
 
 export const LoadingScreenContainer = styled.div`
-  position: fixed; // 화면에 고정
+  position: fixed;
   top: 0;
   left: 0;
-  width: 100%; // 전체 화면 너비
-  height: 100vh; // 전체 화면 높이
+  width: 100%;
+  height: 100vh;
   display: flex;
-  justify-content: center; // 가로 중앙 정렬
-  align-items: center; // 세로 중앙 정렬
-  background-color: rgba(255, 255, 255, 0.9); // 배경색 (선택사항)
-  z-index: 9999; // 다른 요소들 위에 표시
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(255, 255, 255, 0.9);
+  z-index: 9999;
 
-  // 로티 애니메이션이 들어갈 div 스타일링
   div {
-    width: 300px; // 원하는 애니메이션 크기
-    max-width: 90%; // 모바일 대응
+    width: 300px;
+    max-width: 90%;
   }
 `;
 
 export const LoadingTitle = styled.div`
-    padding-top: 150px;
+    margin-bottom: 10px;
     font-size: 30px;
     font-weight: 600;
 `;
