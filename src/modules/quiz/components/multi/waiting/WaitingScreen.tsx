487fe947118a07b7041ac5eca61ae9d360ff064d
yongsoung <dydrkfl223@gmail.com>
2024-11-14 10:46:50 +0900

    Feat: 멀티모드 대기 화면시 나가기 버튼 추가

--- src/modules/quiz/components/multi/waiting/WaitingScreen.tsx
+++ src/modules/quiz/components/multi/waiting/WaitingScreen.tsx
@@ -1,17 +1,38 @@
 import lottie from "lottie-web";
 import { useEffect, useRef } from "react";
-import { WaitingScreenContainer } from "./styled";
+import { WaitingScreenButtonWrap, WaitingScreenContainer } from "./styled";
 import { TeamUserTagProps } from "../../../../../types/room";
+import { SecondaryButtonSmall } from "../../../../../components/buttons/styled";
+import { useGameState } from "../../../../../contexts/GameStateContext/useGameState";
+import { readyRoomSocketEvents } from "../../../../../hook/readyRoomSocketEvent";
+import { useNavigate } from "react-router-dom";
+import { SERVICES } from "../../../../../config/api/constants";
+import { useStompContext } from "../../../../../contexts/StompContext";
+import { useConfirm } from "../../../../../components/confirmPopup";
 
 export const WaitingScreen = ({ teamId }: TeamUserTagProps) => {
     // likecontainer의 타입을 명시적으로 지정 (HTMLDivElement | null)
     const likecontainer = useRef<HTMLDivElement | null>(null);
     const animationInstance = useRef<any>(null); // 애니메이션 인스턴스 저장
+    const { roomUserId, _roomId } = useGameState();
+    const { stompClient } = useStompContext();
+    const navigate = useNavigate();
+    const customConfirm = useConfirm();
+
+    const handleLeaveRoom = async () => {
+        const confirmed = await customConfirm("정말 나가시겠습니까?");
+        if (confirmed && roomUserId) {
+            console.log("나감");  // TODO: 방 나감
+            readyRoomSocketEvents.userExitRoom(stompClient, _roomId, roomUserId);
+
+            navigate(SERVICES.MULTI);
+        }
+    }
 
     useEffect(() => {
         if (likecontainer.current) {
 
-            const animationData = teamId ===1
+            const animationData = teamId === 1
                 ? require("../../../../../lottie/waiting.json")
                 : require("../../../../../lottie/waiting_red.json");
 
@@ -34,8 +55,13 @@ export const WaitingScreen = ({ teamId }: TeamUserTagProps) => {
     }, []);
 
     return (
-        <WaitingScreenContainer>
-            <div ref={likecontainer}></div>
-        </WaitingScreenContainer>
+        <div>
+            <WaitingScreenContainer>
+                <div ref={likecontainer}></div>
+            </WaitingScreenContainer>
+            <WaitingScreenButtonWrap>
+                <SecondaryButtonSmall onClick={handleLeaveRoom}>나가기</SecondaryButtonSmall>
+            </WaitingScreenButtonWrap>
+        </div>
     );
 };
--- src/modules/quiz/components/multi/waiting/styled.ts
+++ src/modules/quiz/components/multi/waiting/styled.ts
@@ -8,3 +8,10 @@ export const WaitingScreenContainer = styled.div`
         fill: none; /* svg 배경을 투명하게 설정 */
     }
 `;
+
+export const WaitingScreenButtonWrap = styled.div`
+    display: flex;
+    gap: 8px;
+    justify-content: flex-end;
+    margin-right: 100px;
+`
